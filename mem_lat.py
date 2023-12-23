f65a7a5ea1b6f9d148f36efca6163f48580d826b
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2023-12-23 16:28:04 +0800

    support auto recognize cache size and design measure strategy

--- mem_lat.py
+++ mem_lat.py
@@ -1,31 +1,38 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import sys
-import csv
-from scipy.signal import find_peaks
 
 script = sys.argv[0]
 
-if not len(sys.argv) == 3:
-    sys.exit(f'Usage: python3 {script} input_csv_filename output_png_filename')
+if not len(sys.argv) == 2:
+    sys.exit(f'Usage: python3 {script} work_dir')
+work_dir = sys.argv[1]
 
-csv_filename = sys.argv[1]
-png_filename = sys.argv[2]
+csv_filename = f'{work_dir}/mem_lat.csv'
+png_filename = f'{work_dir}/mem_lat.png'
+info_filename = f'{work_dir}/info.txt'
+
+with open(info_filename, 'r') as file:
+    info = file.read()
+
+fig = plt.figure(figsize=(12, 5))
+gs = gridspec.GridSpec(1, 2, width_ratios=[8, 2])
+ax1 = plt.subplot(gs[0])
+ax2 = plt.subplot(gs[1])
 
 df = pd.read_csv(csv_filename)
-
 columns = df.columns[1:]
-
 for column in columns:
-    plt.plot(df['buffer_size'], df[column], label=column)
+    ax1.plot(df['buffer_size'], df[column], label=column)
 
-plt.legend()
-plt.title('Latency vs Size in different stride')
-plt.xlabel('Size')
-plt.ylabel('Latency')
+ax1.legend()
+ax1.set_title('Latency vs Size in different stride')
+ax1.set_xlabel('Size(kb)')
+ax1.set_ylabel('Latency(ns)')
+ax2.text(0.5, 0.5, info, fontsize=12, ha='center', va='center')
+ax2.axis('off')
 
 plt.savefig(png_filename)
-
 plt.show()
-
--- mem_lat.sh
+++ mem_lat.sh
@@ -2,30 +2,185 @@
 
 work=./mem_lat
 
-# l1 data cache size 32kb per core, 8 way
-# l2 cache size 256kb per core, 4 way
-# l3 cache size 9 * 1024 kb, 12 way
+function print_usage() {
+	RED='\033[0;31m'
+	BLUE='\033[0;34m'
+	BOLD='\033[1m'
+	NONE='\033[0m'
 
-# buffer size(kb)
-buffer_size_begin=16
-buffer_size_end=10240
+  echo -e "\n${RED}Usage${NONE}: ${BOLD}$0${NONE} [OPTION] [FULL/FAST]"
 
-# stride choices
-# stride=64, default cache line size
-# stride=256, default cache line size * 4(l2 cache is 4 way associate)
-strides=(64 128 256 512)
+  echo -e "\n${RED}OPTIONS${NONE}:
+  ${BLUE}l1${NONE}    test l1 cache latency
+  ${BLUE}l2${NONE}    test l2 cache latency
+  ${BLUE}l3${NONE}    test l3 cache latency
+  ${BLUE}all${NONE}   test all cache latency(default)"
+
+  echo -e "\n${RED}FULL/FAST${NONE}:
+  ${BLUE}full${NONE}   test with small interval and long time
+  ${BLUE}fast${NONE}   test with large interval and short time(default)
+  "
+}
+
+function print_cache_info() {
+	RED='\033[0;31m'
+	BLUE='\033[0;34m'
+	BOLD='\033[1m'
+	NONE='\033[0m'
+
+  echo -e "${RED}Cache Info${NONE}:
+  ${BLUE}l1 cache size${NONE}    $l1_cache_size KB
+  ${BLUE}l1 cache ways${NONE}    $l1_cache_ways
+  ${BLUE}l2 cache size${NONE}    $l2_cache_size KB
+  ${BLUE}l2 cache ways${NONE}    $l2_cache_ways
+  ${BLUE}l3 cache size${NONE}    $l3_cache_size KB
+  ${BLUE}l3 cache ways${NONE}    $l3_cache_ways
+  "
+}
+
+function save_info() {
+  echo -e "Command: $0 $option $mode" >> $dir/info.txt
+  echo -e "Time: $(date +"%Y-%m-%d %H:%M:%S")" >> $dir/info.txt
+  echo -e "Duration: $duration" >> $dir/info.txt
+  echo -e "Hostname: $(hostname)" >> $dir/info.txt
+  echo -e "CPU model:
+  $(cat /proc/cpuinfo | grep "model name" | uniq | cut -d':' -f2-)" >> $dir/info.txt
+  echo -e "Cache Info:
+  l1 cache size    $l1_cache_size KB
+  l1 cache ways    $l1_cache_ways
+  l2 cache size    $l2_cache_size KB
+  l2 cache ways    $l2_cache_ways
+  l3 cache size    $l3_cache_size KB
+  l3 cache ways    $l3_cache_ways
+  " >> $dir/info.txt
+}
+
+if [[ $# -lt 1 ]]; then $1 = "all"; fi
+if [[ $1 != "l1" && $1 != "l2" && $1 != "l3" && $1 != "all" ]]; then
+  print_usage
+  exit 1
+fi
+option=$1
+
+if [[ $# -lt 2 ]]; then $2 = "fast"; fi
+if [[ $2 != *"full"* && $2 != *"fast"* ]]; then
+  print_usage
+  exit 1
+fi
+mode=$2
 
 # create a dir named as timestamp
 dir=results/mem_lat_$(date +"%Y%m%d%H%M%S")
 mkdir -p $dir
 
-for stride in ${strides[@]}
+# collect cache information(cache size is in KB)
+l1_cache_size=$(cat /sys/devices/system/cpu/cpu0/cache/index0/size)
+l1_cache_size=${l1_cache_size%K}
+l1_cache_size=$((l1_cache_size))
+l1_cache_ways=$(cat /sys/devices/system/cpu/cpu0/cache/index0/ways_of_associativity)
+l2_cache_size=$(cat /sys/devices/system/cpu/cpu0/cache/index2/size)
+l2_cache_size=${l2_cache_size%K}
+l2_cache_size=$((l2_cache_size))
+l2_cache_ways=$(cat /sys/devices/system/cpu/cpu0/cache/index2/ways_of_associativity)
+l3_cache_size=$(cat /sys/devices/system/cpu/cpu0/cache/index3/size)
+l3_cache_size=${l3_cache_size%K}
+l3_cache_size=$((l3_cache_size))
+l3_cache_ways=$(cat /sys/devices/system/cpu/cpu0/cache/index3/ways_of_associativity)
+print_cache_info
+
+l1_buffer_size_begin=$((l1_cache_size/2))
+l1_buffer_size_end=$((l1_cache_size+l1_cache_size/2))
+l2_buffer_size_begin=$((l2_cache_size/2))
+l2_buffer_size_end=$((l2_cache_size+l2_cache_size/2))
+l3_buffer_size_begin=$((l3_cache_size/2))
+l3_buffer_size_end=$((l3_cache_size+l3_cache_size/2))
+# make sure l1_buffer_size_end is not larger than l2_buffer_size_begin, so as l2_buffer_size_end and l3_buffer_size_begin.
+if [[ $l1_buffer_size_end -gt $l2_buffer_size_begin ]]; then
+  l1_buffer_size_end=$l2_buffer_size_begin
+fi
+if [[ $l2_buffer_size_end -gt $l3_buffer_size_begin ]]; then
+  l2_buffer_size_end=$l3_buffer_size_begin
+fi
+
+# measure cache latency by range, different range has different interval
+buffer_size_range_begins=($l1_buffer_size_begin $l1_buffer_size_end $l2_buffer_size_begin $l2_buffer_size_end $l3_buffer_size_begin)
+buffer_size_range_ends=($l1_buffer_size_end $l2_buffer_size_begin $l2_buffer_size_end $l3_buffer_size_begin $l3_buffer_size_end)
+buffer_size_range_options=("l1 all" "all" "l2 all" "all" "l3 all")
+full_mode_intervals=(2 4 4 8 8)
+fast_mode_intervals=(2 8 16 32 64)
+# stride choices, default cache line size is 64 bytes
+strides=(64 128 256 512)
+
+if [[ $mode == *"full"* ]]; then
+  intervals=(${full_mode_intervals[@]})
+else
+  intervals=(${fast_mode_intervals[@]})
+fi
+
+# measure cache latency by running $work
+start_time=$(date +%s)
+for stride in ${strides[@]};
 do
-  for buffer_size in $(seq $buffer_size_begin 4 $buffer_size_end); do
-      taskset -ac 2 $work -b $buffer_size -s $stride
-  done > $dir/mem_lat_$stride.log
+  for ((i = 0; i < ${#buffer_size_range_begins[@]}; i++)); do
+    buffer_size_begin=${buffer_size_range_begins[$i]}
+    buffer_size_end=${buffer_size_range_ends[$i]}
+    options=${buffer_size_range_options[$i]}
+    interval=${intervals[$i]}
+    if grep -q $option <<< $options; then
+      for buffer_size in $(seq $buffer_size_begin $interval $buffer_size_end); do
+        # set core affinity
+        taskset -ac 2 $work -b $buffer_size -s $stride
+      done >> $dir/mem_lat_$stride.log
+    fi
+  done
+  echo "Stride $stride finished"
 done
 
-paste -d' ' $dir/mem_lat_64.log $dir/mem_lat_128.log $dir/mem_lat_256.log $dir/mem_lat_512.log > $dir/mem_lat.log
-cat $dir/mem_lat.log | awk 'BEGIN{print "buffer_size,stride 64,stride 128,stride 256,stride 512"} {print $3 "," $11 "," $23 "," $35 "," $47}' > $dir/mem_lat.csv
+# measure test duration
+end_time=$(date +%s)
+duration=$((end_time - start_time))
+hours=$((duration / 3600))
+minutes=$((duration % 3600 / 60))
+seconds=$((duration % 60))
+duration=""
+if [[ $hours -gt 0 ]]; then
+  duration="$hours hours,"
+fi
+if [[ $minutes -gt 0 ]]; then
+  duration="$duration $minutes minutes,"
+fi
+duration="$duration $seconds seconds"
+echo "Measurement duration: $duration"
+save_info
 
+# merge all log files
+for stride in ${strides[@]}; do
+  echo "$dir/mem_lat_$stride.log"
+done | xargs paste -d' ' > $dir/mem_lat.log
+
+# prepare csv header
+csv_header="buffer_size"
+for stride in ${strides[@]};
+do
+  csv_header="$csv_header,stride $stride"
+done
+
+# calculate the column number of each stride
+csv_cols="3"
+for i in $(seq 1 ${#strides[@]})
+do
+  col=$((i*12-1))
+  csv_cols="$csv_cols $col"
+done
+
+# convert to csv format
+cat $dir/mem_lat.log | awk -v csv_header="$csv_header" -v csv_cols="$csv_cols" \
+  'BEGIN{print csv_header; FS=" "; OFS=","; split(csv_cols, cols, " ");} \
+  {for (i in cols) {printf "%s%s", $cols[i], (i==length(cols)?"\n":OFS);}}' \
+> $dir/mem_lat.csv
+
+echo "Results are saved in $dir/mem_lat.csv"
+
+# draw the graph
+python3 ./mem_lat.py $dir
+echo "Image are saved in $dir/mem_lat.png"
